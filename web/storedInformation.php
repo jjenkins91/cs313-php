6fb16b0aa2880f03a4da62901f4900010635bf21
Jason Jenkins <jasonjenkins@jasons-mbp.myfiosgateway.com>
2020-10-16 21:27:23 -0400

    week 05 prove

--- web/storedInformation.php
+++ web/storedInformation.php
@@ -46,50 +46,14 @@ session_start();
   <h1 class="confirmationTitle">Address Confirmation</h1>
 </div>
 <div class="bodyContentConfirmation">
-  <h1>
- <?php
-$address1 = $_POST["address1"];
-echo $address1 . ' ';
-$address2 = $_POST["address2"];
-echo $address2 . ' ';
-$city = $_POST["city"];
-echo $city . ', ';
-$state = $_POST["state"];
-echo $state . ' ';
-$zipCode = $_POST["zipCode"];
-echo $zipCode;
-  ?>
-  </h1>
-</div>
-<div class="orderConfirmationTitleSpace">
-  <h1 class="orderConfirmationTitle">Order Summary</h1>
-</div>
-
-<div class="orderConfirmationSpace">
-  <h1 class="buttonOne">
-<!-- <img class="taurus" src="KLtaurus.jpeg" alt="taurusPolish"> -->
-<?php
-if ($_SESSION["button1"]) {
-  echo $_SESSION["button1"];
-  echo '<img src="Taurus.jpeg" alt="icon" />';
-}
-
-if ($_SESSION["button2"]) {
-  echo $_SESSION["button2"];
-  echo '<img src="coconutMilk.jpeg" alt="icon" />';
-}
-
-if ($_SESSION["button3"]) {
-  echo $_SESSION["button3"];
-  echo '<img src="KLgold.jpeg" alt="icon" />';
-}
-
-if ($_SESSION["button4"]) {
-  echo $_SESSION["button4"];
-  echo '<img src="KLmiami.jpeg" alt="icon" />';
-}
-?>
-  </h1>
+  <?php
+  foreach ($db->query('SELECT username, password FROM customer_information') as $row)
+  {
+    echo 'user: ' . $row['username'];
+    echo ' password: ' . $row['password'];
+    echo '<br/>';
+  }
+   ?>
 </div>
 <div class="footer">
   <h3 class="copyright">Copyright© October 2020 Jason Jenkins</h3>
