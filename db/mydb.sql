5d20c51869b0f926faac7ee02b911ec0656c3f74
Jason Jenkins <jasonjenkins@jasons-mbp.myfiosgateway.com>
2020-10-30 21:19:29 -0400

    week 07 prove

--- db/mydb.sql
+++ db/mydb.sql
@@ -1,32 +1,31 @@
-CREATE TABLE customer_information (
-	customer_id INT PRIMARY KEY,
-	customer_name VARCHAR(80),
-	customer_username VARCHAR(80),
-	customer_password VARCHAR(80)
+CREATE TABLE customer_info (
+	customer_id SERIAL PRIMARY KEY,
+	customer_first_name VARCHAR(80),
+	customer_last_name VARCHAR(80	)
 );
 
-CREATE TABLE address (
-	address_id INT PRIMARY KEY,
-	customer_id INT REFERENCES customer_information (customer_id),
+CREATE TABLE address2 (
+	address_id SERIAL PRIMARY KEY,
+	customer_id INT REFERENCES customer_info (customer_id),
 	address_street VARCHAR(80),
 	address_city VARCHAR(80),
 	address_state VARCHAR(80),
 	address_zip_code INT
 );
 
-CREATE TABLE payment_method (
-	payment_method_id INT PRIMARY KEY,
+CREATE TABLE payment_method1 (
+	payment_method_id SERIAL PRIMARY KEY,
 	customer_id  INT REFERENCES customer_information (customer_id),
 	payment_card_type VARCHAR(80)
 );
 
 CREATE TABLE product (
-	product_id INT PRIMARY KEY,
+	product_id SERIAL PRIMARY KEY,
 	product_name VARCHAR(80)
 );
 
-CREATE TABLE orders (
-	order_id INT PRIMARY KEY,
-	customer_id  INT REFERENCES customer_information (customer_id),
+CREATE TABLE orders1 (
+	order_id SERIAL PRIMARY KEY,
+	customer_id  INT REFERENCES customer_info (customer_id),
 	product_id  INT REFERENCES product (product_id)
 );
